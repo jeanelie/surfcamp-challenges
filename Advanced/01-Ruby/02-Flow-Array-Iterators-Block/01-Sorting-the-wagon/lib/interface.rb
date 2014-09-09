c7dbdab1eeb93d2c9d9352141ebabb273bbdc0b2
Jean-Élie <jeclic@gmail.com>
2014-09-09 17:23:33 +0200

    sorting the wagon

--- Advanced/01-Ruby/02-Flow-Array-Iterators-Block/01-Sorting-the-wagon/lib/interface.rb
+++ Advanced/01-Ruby/02-Flow-Array-Iterators-Block/01-Sorting-the-wagon/lib/interface.rb
@@ -8,7 +8,25 @@ begin
   name.chomp! if name
 
   # TODO: Add the name we just gotto the students array
+  students << name unless name.empty?
 
 end while name != ""
 
 # TODO: Call `wagon_sort` method and display the sorted student list
+students = wagon_sort(students)
+
+students_string = ""
+students.each_with_index do |student, index|
+
+  if index == students.size - 2
+    students_string += student + " and "
+  elsif index == students.size - 1
+    students_string += student
+  else
+    students_string += student + ", "
+  end
+
+end
+
+puts "Congratulations! Your Wagon has #{students.size} students:
+- #{students_string}"
