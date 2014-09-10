749ee0692a7353ec064fac2f9f62ceabf2a718eb
Jean-Élie <jeclic@gmail.com>
2014-09-10 16:16:13 +0200

    pseudo code

--- Advanced/01-Ruby/03-Hash-Symbols/03-Numbers-and-Letters/lib/longest_word.rb
+++ Advanced/01-Ruby/03-Hash-Symbols/03-Numbers-and-Letters/lib/longest_word.rb
@@ -8,4 +8,38 @@ end
 
 def run_game(attempt, grid, start_time, end_time)
   #TODO: runs the game and return detailed hash of result
+
+  message = ""
+  score = 0
+  duration = end_time - start_time
+  if !attempt_is_in_the_grid?(attempt, grid)
+    message = "not in the grid"
+  else
+    translation = translate(attempt)
+    if translation.length == 0
+      message = "not an english word"
+    else
+      message = "well done"
+      score = compute_score(attempt, duration)
+    end
+  end
+
+  result = {
+    time: duration,
+    translation: translation,
+    score: score,
+    message: message
+  }
+end
+
+def attempt_is_in_the_grid?(attempt, grid)
+
+end
+
+def translate(attempt)
+
+end
+
+def compute_score(attempt, duration)
+  score = attempt.size / duration
 end
