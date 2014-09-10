5e7ebb3c6445b14a427dfc824e3258736b5fb68a
Jean-Élie <jeclic@gmail.com>
2014-09-10 15:37:51 +0200

    generate random letters

--- Advanced/01-Ruby/03-Hash-Symbols/03-Numbers-and-Letters/lib/longest_word.rb
+++ Advanced/01-Ruby/03-Hash-Symbols/03-Numbers-and-Letters/lib/longest_word.rb
@@ -3,6 +3,7 @@ require 'json'
 
 def generate_grid(grid_size)
   #TODO: generate random grid of letters
+  (0...grid_size).map { ('a'..'z').to_a[rand(26)] }
 end
 
 def run_game(attempt, grid, start_time, end_time)
