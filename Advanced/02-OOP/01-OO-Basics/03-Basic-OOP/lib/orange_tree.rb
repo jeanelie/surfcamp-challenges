ced0b2b6204c3d7221cd7ba4a31c067a38861ba7
Jean-Élie <jeclic@gmail.com>
2014-09-11 10:56:08 +0200

    lemon tree

--- Advanced/02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
+++ Advanced/02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
@@ -1,3 +1,39 @@
 class OrangeTree
-  # TODO: Implement all the specs defined in the README.md :)
+
+  attr_reader :height, :fruits
+
+  def initialize
+    @age = 0
+    @height = 0
+    @fruits = 0
+  end
+
+  def one_year_passes!
+    @age += 1
+
+    if @age > 10 && @age <= 15
+      @fruits = 200
+    elsif @age > 5 && @age <= 10
+      @fruits = 100
+    else
+      @fruits = 0
+    end
+
+    if @age <= 10
+      @height += 1
+    end
+  end
+
+  def pick_a_fruit!
+    @fruits -= 1
+  end
+
+  def dead?
+    if @age <= 50
+      false
+    else @age > 100
+      true
+    end
+  end
+
 end
