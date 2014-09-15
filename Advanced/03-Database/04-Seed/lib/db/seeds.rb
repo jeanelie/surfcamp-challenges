925b13e5e310451abce5432907c3e3cb41683fe3
Jean-Élie <jeclic@gmail.com>
2014-09-15 18:47:41 +0200

    exo 3 db

--- Advanced/03-Database/04-Seed/lib/db/seeds.rb
+++ Advanced/03-Database/04-Seed/lib/db/seeds.rb
@@ -10,4 +10,40 @@ puts 'Seeding database...'
 
 # TODO: Your code goes here
 # 1. create fake users
+for i in 1..10 do
+  User.create(name: Faker::Name.name, email: Faker::Internet.email)
+end
+
+users = User.all
+
 # 2. Scrape news from https://news.ycombinator.com/ and associate posts to your existing users
+doc = Nokogiri::HTML(open("https://news.ycombinator.com/"))
+
+# name, source url, rating and creation date
+titles = doc.css("td.title a").map { |title| title.text }
+# titles.each do |title|
+#   puts title.text
+# end
+
+urls = doc.css("td.title a").map{ |url| url['href'] }
+# urls.each do |url|
+#   puts url['href']
+# end
+
+points = doc.css("td.subtext span").map { |point| point.text.tr('^0-9', '') }
+# points.each do |point|
+#   puts point.text
+# end
+
+titles.each_with_index do |title, index|
+  params = {
+    name: title,
+    date: Time.now,
+    source_url: urls[index],
+    rating: points[index] == nil ? 0 : points[index],
+    user: users[index % users.size]
+  }
+
+  puts params
+  Post.create(params)
+end
--- Advanced/03-Database/04-Seed/lib/interface.rb
+++ Advanced/03-Database/04-Seed/lib/interface.rb
@@ -3,3 +3,42 @@ require_relative 'models/post'
 require_relative 'models/user'
 
 # TODO: build your interface with login and CRUD tasks on the posts
+def ask(prompt)
+  print "#{prompt} "
+  gets.to_s.chomp
+end
+
+def list_posts(user)
+  # TODO: use ActiveRecord to get all posts of the current user
+  posts = user.post.all
+
+  puts '-' * 50
+  puts '%-3s %-15s %-20s %-30s %s' % ['#', 'Name', 'Source URL', 'Created At', 'Rating']
+
+  posts.each do |post|
+    puts '%-3d %-15s %-20s %-30s %d' % [post.id, post.name, post.source_url, post.date, post.rating]
+  end
+
+  puts '-' * 50
+end
+
+logged_in = false
+while true
+
+  until logged_in
+    puts 'Please login with your <id>'
+    # TODO: instantiate a user with his <id>
+    user_id = ask(">")
+    user = User.where(email: user_id).take
+    logged_in = true if user != nil
+  end
+
+  puts "Hey #{user.name}, what do you want to do today? Enter <task_id>"
+  puts "1. Read your posts"
+  puts "2. Exit"
+
+  case ask('>').to_i
+  when 1 then list_posts(user)
+  when 2 then break
+  end
+end
